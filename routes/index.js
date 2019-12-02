244b9f05ed502a5af01cd0a46e632cd7ab4d0785
Kunj Patel <kunjp188@gmail.com>
2019-12-02 10:51:47 -0500

    Converting to pug

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/league', function(req, res, next) {
+  res.render('league', { title: 'Express' });
+});
+
+router.get('/matchup', function(req, res, next) {
+  res.render('matchup', { title: 'Express' });
+});
+
+router.get('/players', function(req, res, next) {
+  res.render('players', { title: 'Express' });
+});
+
+router.get('/team', function(req, res, next) {
+  res.render('team', { title: 'Express' });
+});
+
+router.get('/trades', function(req, res, next) {
+  res.render('trades', { title: 'Express' });
+});
+
 module.exports = router;
